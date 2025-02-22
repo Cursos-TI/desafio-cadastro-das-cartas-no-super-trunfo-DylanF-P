a1f46ecf3c0de95d76c83b9fc4e34200a9ea6aa5
DylanF-P <dylanfidelisdylanfidelis@gmail.com>
2025-02-22 00:33:16 +0000

    Criei o codigo das duas cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,83 @@
 //Teste larissa
 
 int main() {
+
+    int populacao, turisticos;
+    float pib, area;
+    char nome[50], codigo[50];
+    char estado[20];
+    int populacao2, turisticos2;
+    float pib2, area2;
+    char nome2[50], codigo2[50];
+    char estado2[20];
+
+    printf("Digite o nome do Estado: ");
+    scanf("%s", &estado);
+
+    printf("Digite o Código da Cidade: ");
+    scanf("%s", &codigo);
+
+    printf("Digite o nome da Cidade: ");
+    scanf("%s", &nome);
+
+    printf("Digite a população: ");
+    scanf("%d", &populacao);
+
+    printf("Digite a Área Total em KM2: ");
+    scanf("%f", &area);
+
+    printf("Digite o PIB: ");
+    scanf("%f", &pib);
+
+    printf("Digite a quantidade de pontos turísticos: ");
+    scanf("%d", &turisticos);
+
+    printf("Digite o nome do Segundo Estado: ");
+    scanf("%s", &estado2);
+
+    printf("Digite o Código da Cidade: ");
+    scanf("%s", &codigo2);
+
+    printf("Digite o nome da Cidade: ");
+    scanf("%s", &nome2);
+
+    printf("Digite a população: ");
+    scanf("%d", &populacao2);
+
+    printf("Digite a Área Total em KM2: ");
+    scanf("%f", &area2);
+
+    printf("Digite o PIB: ");
+    scanf("%f", &pib2);
+
+    printf("Digite a quantidade de pontos turísticos: ");
+    scanf("%d", &turisticos2);
+
+
+    printf("Estado: %s \n", estado);
+    printf("Código: %s \n", codigo);
+    printf("Nome da Cidade: %s \n", nome);
+    printf("População: %d \n", populacao);
+    printf("Área: %f  \n", area);
+    printf("PIB: %f \n", pib);
+    printf("Número de Pontos Turísticos: %d \n", turisticos);
+
+    printf("Estado: %s \n", estado2);
+    printf("Código: %s \n", codigo2);
+    printf("Nome da Cidade: %s \n", nome2);
+    printf("População: %d \n", populacao2);
+    printf("Área: %f  \n", area2);
+    printf("PIB: %f \n", pib2);
+    printf("Número de Pontos Turísticos: %d \n", turisticos2);
+
+
+
+
+
+
+
+
+
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
